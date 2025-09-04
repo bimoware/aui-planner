d6cd0ad6daf82b05faeb331aaae85881b57278c1
Malik Lahlou <71925280+bimoware@users.noreply.github.com>
2025-09-04 04:04:29 +0200

    ✨ Added time labels

--- src/cpn/CalendarSVG/index.tsx
+++ src/cpn/CalendarSVG/index.tsx
@@ -8,18 +8,19 @@ export default function CalendarSVG({ elems = [] }: { elems?: Element[] }) {
     const maxEnd = Math.max(...elems.map(e => e.end.h))
 
     const padding = 2
-    const columnWidth = 20
-    const rowHeight = 10
-    const width = padding * 2 + columnWidth * 5
-    const height = padding * 2 + rowHeight * (maxEnd - minStart)
+    const leftPadding = 3
+    const columnWidth = 15
+    const rowHeight = 15
+    const width = padding + leftPadding + columnWidth * 5 + padding
+    const height = padding + rowHeight * (maxEnd - minStart) + padding
 
     return <svg viewBox={`0 0 ${width} ${height}`} className="w-full h-full">
+        <g id="v-lines" opacity={0.5}>
         {
             // The 4 lines separating the 5 work days (the vertical lines)
-            getMiddlePoints(0, 5, 4)
-                .map(id => {
-                    const x = padding + columnWidth * id
-                    return <line key={id}
+            getMiddlePoints(padding + leftPadding, width - leftPadding,4)
+                .map(x => {
+                    return <line key={x}
                         x1={x}
                         x2={x}
                         y1={padding}
@@ -29,10 +30,12 @@ export default function CalendarSVG({ elems = [] }: { elems?: Element[] }) {
                     />
                 })
         }
+        </g>
+        <g id="weekdays-labels">
         {
             // The name of the days (the text between the vertical lines)
             ["M", "T", "W", "R", "F"].map((day, i) => {
-                const x = padding + columnWidth * i + columnWidth / 2
+                const x = padding + leftPadding + columnWidth * i + columnWidth / 2
                 return <text key={day}
                     x={x}
                     y={padding}
@@ -45,13 +48,15 @@ export default function CalendarSVG({ elems = [] }: { elems?: Element[] }) {
                 </text>
             })
         }
+        </g>
+        <g id="h-lines" opacity={0.5}>
         {
             // All hours around the times (the horizental lines)
-            getMiddlePoints(minStart, maxEnd, 4)
+            getMiddlePoints(minStart, maxEnd, maxEnd - minStart - 1)
                 .map(id => {
                     const y = padding + rowHeight * (id - minStart)
                     return <line key={id}
-                        x1={padding}
+                        x1={padding + leftPadding}
                         x2={width - padding}
                         y1={y}
                         y2={y}
@@ -59,7 +64,26 @@ export default function CalendarSVG({ elems = [] }: { elems?: Element[] }) {
                         strokeWidth={0.2}
                     />
                 })
-                
+
         }
+        </g>
+        <g id="time-labels">
+            {
+            getMiddlePoints(minStart, maxEnd, maxEnd - minStart - 1)
+            .map(id =>{
+                const y = padding + rowHeight * (id - minStart)
+                return <text key={id}
+                    x={padding}
+                    y={y}
+                    fill="white"
+                    fontSize={2}
+                    textAnchor="end"
+                    alignmentBaseline="middle"
+                >
+                    {id}:00
+                </text>
+            })
+            }
+        </g>
     </svg>
 }
