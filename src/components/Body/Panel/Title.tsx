64d7cf26e25cf2d3291ee0f50db06dbea8b8f5c0
Malik Lahlou <71925280+bimoware@users.noreply.github.com>
2025-09-13 21:46:12 +0200

    ✨ Added BETA Icon

--- src/components/Body/Panel/Title.tsx
+++ src/components/Body/Panel/Title.tsx
@@ -1,8 +1,23 @@
+import { LoaderCircle } from "@/components/animate-ui/icons/loader-circle";
 import CalendarIcon from "../../icons/CalendarIcon";
+import { CctvIcon } from "@/components/animate-ui/icons/cctv";
 
 export function Title() {
-    return <h1 className="inline-flex gap-3 w-full justify-center">
+    return <h1 className="inline-flex gap-3 w-full justify-center relative">
         <CalendarIcon />
         Malik's AUI Planner
+        <BetaBadge />
     </h1>
+}
+
+function BetaBadge() {
+    return <div className="absolute bg-purple-900
+    -top-4 -right-4 text-xl
+    px-2 p-1 rounded-lg opacity-80
+    rotate-6
+    select-none hover:scale-110 duration-100
+    flex gap-2">
+        <CctvIcon animateOnHover className="h-lh"/>
+        BETA
+    </div>
 }
